1493041222468f4821fabd7474aea9260a389b98
Michael Roncone <mjroncone@gmail.com>
2015-06-19 20:18:30 -0500

    add: personal information to be presented on resume

---
+++ js/resumeBuilder.js
@@ -0,0 +1,128 @@
+// All hard-coded biographical information
+var bio = {
+   'name' : 'Mike Roncone',
+   'role' : 'Front End Web Developer in Progress',
+   'contactInfo' : {
+      'mobile' : '630-674-1586',
+      'email' : 'mjroncone@gmail.com',
+      'github' : 'https://github.com/mjroncone',
+      'twitter' : 'https://twitter.com/MikeRoncone',
+      'location' : 'Chicago'
+   },
+   'bioPic' : 'images/face.jpg',
+   'welcomeMessage' : 'Howdy!',
+   'skills' : ['HTML ', 'CSS ', 'JavaScript ', 'jQuery']
+}
+
+// All hard-coded work history information
+var work = {
+   'jobs' : [
+      {
+         'workTitle' : 'Trader Trainee',
+         'workEmployer' : 'Liquid Capital Markets',
+         'workDates' : 'December 2014 - Present',
+         'workLocation' : 'Chicago, Illinois',
+         'workDescription' : 'Filter any information relevant to the desk and' +
+                              'ensure all traders have been informed quickly and' +
+                              'accurately. Reconcile Profit and Loss prints while' +
+                              'traders adjust our pricing models. Act with extreme' +
+                              'accuracy in a high pressure environment while' +
+                              'processing inputs from many sources at once. Recognize' +
+                              'and follow pricing trends over numerous different' +
+                              'product expirations in terms of both intraday and' +
+                              'macro order flow to assess the level of risk associated' +
+                              'with different terms.'
+      },
+      {
+         'workTitle' : 'Treasury Options Clerk',
+         'workEmployer' : 'AHJ & R Brokerage',
+         'workDates' : 'April 2013 - December 2014',
+         'workLocation' : 'Chicago, Illinois'
+         'work Description' : 'Relayed quotes and orders from customers over the' +
+                              'phone to brokers in the pit for execution. Reconciled' +
+                              'trades to insure proper filling of brokerage orders,' +
+                              'reconciled out trades, sent customer fill alerts,' +
+                              'and delivered order tickets to customer representatives' +
+                              'on the floor. Learned options theories to aid in the' +
+                              'understanding, checking, and execution of trades.' +
+                              'Redesigned monthly and daily tracking Excel spreadsheets' +
+                              'to automate repetitive tasks & reduce errors'
+      },
+      {
+         'workTitle' : 'Cryptocurrency Market Maker',
+         'workEmployer' : 'Inependent',
+         'workDates' : 'January 2013 - February 2014',
+         'workLocation' : 'Chicago, Illinois'
+         'workDescription' : 'Created bids and offers for Bitcoin (BTC) and' +
+                              'Ripple (XRP) against the US dollar based on a' +
+                              'combination of synthetic pricing using inter-market' +
+                              'spreads, historical volatility, and order flow.' +
+                              'Arbitraged inter-exchange spreads due to a lack of' +
+                              'emerging market efficiency and the presence of a' +
+                              'multitude of independent exchanges. Provided essential' +
+                              'liquidity in virtually non-existent markets.'
+      },
+      {
+         'workTitle' : 'Client Service Assistant',
+         'workEmployer' : 'Altair Advisers',
+         'workDates' : 'April 2013 - December 2014',
+         'workLocation' : 'Chicago, Illinois',
+         'workDescription' : 'Managed recurring and ad-hoc client projects &' +
+                              'automated processes or created templates where' +
+                              'possible. Monitored and updated monthly and quarterly' +
+                              'performance documents to ensure accuracy and integrity' +
+                              'of data. Helped form strategies to organize and' +
+                              'utilize a more effective database, automating' +
+                              'numerous processes saving time, costs, and creating' +
+                              'better client management/engagement. Sharpened' +
+                              'Microsoft Excel skills, learning numerous new functions' +
+                              'in order to speed up processing time. Conducted' +
+                              'ad-hoc research and analysis for consultants.'
+      },
+      {
+         'workTitle' : 'Shift Supervisor',
+         'workEmployer' : 'Panera Bread',
+         'workDates' : 'August 2008 - January 2012',
+         'workLocation' : 'Schaumburg, Illinois',
+         'workDescription' : 'Managed labor flows to accommodate for constantly' +
+                              'changing needs of the store during shifts. Analyzed' +
+                              'labor costs and coordinated schedules to lower' +
+                              'variance between needed and actual labor hours.' +
+                              'Analyzed food costs and prepared estimates for' +
+                              'needed bakery pan-ups/food preparation per day.'
+      }
+   ]
+}
+
+// All hard-coded project information
+var projects = {
+   'projects' = [
+      {
+         'projectTitle' : 'Portfolio Website',
+         'projectDates' : '2015',
+         'projectDescription' : 'Built anonline portfolio using HTML, CSS, and some javascript.'
+         'projectImage' : 'images/gwscompass.svg'
+      }
+   ]
+}
+
+// All hard-coded education information
+var education = {
+   'schools' : [
+      {
+      'schoolName' : 'Depaul University',
+      'schoolDates' : 'September 2010 - June 2014',
+      'schoolDegree' : 'Bachelors of Science',
+      'schoolMajor' : 'Finance',
+      'schoolLocation' : 'Chicago, Illinois'
+   }
+],
+   'onlineCourses' : [
+      {
+      'title' : 'Nanodegree in Front End Web Development',
+      'school' : 'Udacity',
+      'dates' : 'June 2015 - July 2015',
+      'url' : 'https://www.udacity.com/course/front-end-web-developer-nanodegree--nd001'
+   }
+]
+}
